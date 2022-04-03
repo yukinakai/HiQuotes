764c0909cc23eb4783488668534ee6a952e738f5
yukinakai <10libra20.y@gmail.com>
2022-04-04 08:49:05 +0900

    increment dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: dotenv.get('SAMPLE_KEY'),
       theme: ThemeData(
         // This is the theme of your application.
         //
